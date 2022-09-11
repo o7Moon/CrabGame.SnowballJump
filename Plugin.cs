50daf32c37d5459284bfe61efe7d8072009e2954
o7Moon <0167moon@gmail.com>
2022-09-11 13:06:23 -0500

    fix ping related issues and improve comments

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,7 @@ namespace snowballJump
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
     {
-        // equipping the snowball (slot 4, index 3) should only be allowed when done automaticcally by code
+        // equipping the snowball (slot 4, index 3) should only be allowed when done automatically by code
         public static bool canEquipSnow = false;
         public static int swap_slot_timer = 0;
         // the slot index to swap back to after throwing the snowball
@@ -19,6 +19,7 @@ namespace snowballJump
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
+        //                   Snowball
         [HarmonyPatch(typeof(MonoBehaviour1PublicTrtrGahiRiCoBoItVeBoUnique),"OnCollisionEnter")]
         [HarmonyPostfix]
         public static void snowball_hit_patch(MonoBehaviour1PublicTrtrGahiRiCoBoItVeBoUnique __instance, Collision param_1){
@@ -30,6 +31,7 @@ namespace snowballJump
                 if (r != null) r.AddExplosionForce(700f,param_1.contacts[0].point,10f,0f,ForceMode.Impulse);
             }
         }
+        //                   PlayerInventory
         [HarmonyPatch(typeof(MonoBehaviourPublicItDi2ObIninInTrweGaUnique),"EquipItem")]
         [HarmonyPrefix]
         public static bool equipPre(MonoBehaviourPublicItDi2ObIninInTrweGaUnique __instance, int param_1){
@@ -42,6 +44,7 @@ namespace snowballJump
             }
             return true;
         }
+        //                   PlayerInventory
         [HarmonyPatch(typeof(MonoBehaviourPublicItDi2ObIninInTrweGaUnique),"EquipItem")]
         [HarmonyPostfix]
         public static void equipPost(MonoBehaviourPublicItDi2ObIninInTrweGaUnique __instance){
@@ -60,8 +63,10 @@ namespace snowballJump
                     snowball.GetReady();
                     __instance.playerInventory.UseItem();
                 }
+                // the below issue seems to be ping based, so get the ping and use that for the delay
+                int ping = (MonoBehaviourPublicStLi1InInUnique.GetPing())/15 + 3;
                 // swap back after some amount of FixedUpdate frames, to fix an issue where the snowball doesnt get thrown client-side but does in multiplayer.
-                swap_slot_timer = 10;
+                swap_slot_timer = ping;
             }
         }
         [HarmonyPatch(typeof(PlayerInput),"FixedUpdate")]
